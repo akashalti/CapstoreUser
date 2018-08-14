e243f7769c474fa989796d1c7d54f40d79d26935
Lalit Nikhare <lalitn246@gmail.com>
2018-08-14 14:16:11 +0530

    Renamed packages

--- src/main/java/com/capgemini/userstore/boot/UserstoreApplication.java
+++ src/main/java/com/capgemini/userstore/boot/UserstoreApplication.java
@@ -1,4 +1,4 @@
-package com.capgemini.userstore;
+package com.capgemini.userstore.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
