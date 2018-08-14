67fd9fc4192afcb49e14c8f9739c119cd9fc6fb3
Lalit Nikhare <lalitn246@gmail.com>
2018-08-14 11:36:31 +0530

    Initial Project Structure

---
+++ src/main/java/com/capgemini/userstore/UserstoreApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.userstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserstoreApplication.class, args);
+	}
+}
---
+++ src/test/java/com/capgemini/userstore/UserstoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.capgemini.userstore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserstoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
